f23091365c791e407e178fd60390c35f054e807f
oh-jinsu <ohjinsu98@icloud.com>
2026-01-10 12:04:22 +0900

    Remove the console log

--- src/create_text_editor.tsx
+++ src/create_text_editor.tsx
@@ -1,9 +1,9 @@
 import React, {
-  useImperativeHandle,
-  type DetailedHTMLProps,
-  type FC,
-  type InputHTMLAttributes,
-  type Ref,
+    useImperativeHandle,
+    type DetailedHTMLProps,
+    type FC,
+    type InputHTMLAttributes,
+    type Ref,
 } from "react";
 import { Transaction } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
@@ -13,101 +13,105 @@ import { Subject } from "rxjs";
 import { createCommands } from "./commands";
 import { TextEditorInput } from "./input";
 import {
-  createTextEditorController,
-  type CreateTextEditorOptions,
-  type TextEditorControllerProps,
+    createTextEditorController,
+    type CreateTextEditorOptions,
+    type TextEditorControllerProps,
 } from "./text_editor_controller";
 
 export type TextEditorController = {
-  schema: ReturnType<typeof createSchema>;
-  view: EditorView;
-  subject: Subject<Transaction>;
-  set value(value: string);
-  get value(): string;
+    schema: ReturnType<typeof createSchema>;
+    view: EditorView;
+    subject: Subject<Transaction>;
+    set value(value: string);
+    get value(): string;
 } & { commands: ReturnType<typeof createCommands> };
 
 type HTMLElementProps = DetailedHTMLProps<
-  InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
 >;
 
 export type TextEditorProps = Omit<HTMLElementProps, "ref"> & {
-  ref?: Ref<TextEditorController>;
-  name?: string;
+    ref?: Ref<TextEditorController>;
+    name?: string;
 } & TextEditorControllerProps;
 
 export function createTextEditor(
-  options: CreateTextEditorOptions = {}
+    options: CreateTextEditorOptions = {}
 ): FC<TextEditorProps> {
-  const schema = createSchema();
+    const schema = createSchema();
 
-  function Component({
-    ref,
-    className,
-    autoFocus,
-    onChange,
-    mode,
-    state,
-    editor,
-    defaultValue,
-    updateDelay,
-    placeholder,
-    ...props
-  }: TextEditorProps = {}) {
-    const containerRef = useRef<HTMLDivElement>(null);
+    function Component({
+        ref,
+        className,
+        autoFocus,
+        onChange,
+        mode,
+        state,
+        editor,
+        defaultValue,
+        updateDelay,
+        placeholder,
+        ...props
+    }: TextEditorProps = {}) {
+        const containerRef = useRef<HTMLDivElement>(null);
 
-    const controllerRef = useRef<TextEditorController | null>(null);
+        const controllerRef = useRef<TextEditorController | null>(null);
 
-    useImperativeHandle(
-      ref || controllerRef,
-      () => {
-        const container = containerRef.current!;
+        useImperativeHandle(
+            ref || controllerRef,
+            () => {
+                const container = containerRef.current!;
 
-        const textEditorController = createTextEditorController(
-          container,
-          schema,
-          options,
-          {
-            mode,
-            state,
-            editor,
-            defaultValue,
-            updateDelay,
-            placeholder,
-          }
+                const textEditorController = createTextEditorController(
+                    container,
+                    schema,
+                    options,
+                    {
+                        mode,
+                        state,
+                        editor,
+                        defaultValue,
+                        updateDelay,
+                        placeholder,
+                    }
+                );
+
+                controllerRef.current = textEditorController;
+
+                return textEditorController;
+            },
+            []
         );
 
-        controllerRef.current = textEditorController;
+        useEffect(() => {
+            const controller = controllerRef.current;
 
-        return textEditorController;
-      },
-      []
-    );
+            if (!controller) {
+                return;
+            }
 
-    useEffect(() => {
-      if (autoFocus) {
-        controllerRef.current?.view.focus();
-      }
+            if (autoFocus) {
+                controller.view.focus();
+            }
 
-      return () => {
-        console.log(controllerRef.current);
+            return () => {
+                controller.view.destroy();
+            };
+        }, []);
 
-        controllerRef.current?.view.destroy();
-      };
-    }, []);
+        return (
+            <>
+                <div {...props} ref={containerRef} className={className} />
+                <TextEditorInput
+                    ref={controllerRef}
+                    updateDelay={updateDelay}
+                    defaultValue={defaultValue}
+                    onChange={onChange}
+                />
+            </>
+        );
+    }
 
-    return (
-      <>
-        <div {...props} ref={containerRef} className={className} />
-        <TextEditorInput
-          ref={controllerRef}
-          updateDelay={updateDelay}
-          defaultValue={defaultValue}
-          onChange={onChange}
-        />
-      </>
-    );
-  }
-
-  return Component;
+    return Component;
 }
