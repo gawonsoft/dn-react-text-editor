e1594f21cebcfcd36e455cc8937fd4e2a43e5008
oh-jinsu <ohjinsu98@icloud.com>
2026-02-27 18:29:56 +0900

    Use layout effect

--- src/text_editor.tsx
+++ src/text_editor.tsx
@@ -1,85 +1,86 @@
 import {
-  useImperativeHandle,
-  useMemo,
-  type DetailedHTMLProps,
-  type InputHTMLAttributes,
-  type Ref,
+    useImperativeHandle,
+    useLayoutEffect,
+    useMemo,
+    type DetailedHTMLProps,
+    type InputHTMLAttributes,
+    type Ref,
 } from "react";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { TextEditorInput } from "./input";
 import {
-  TextEditorController,
-  type TextEditorControllerProps,
+    TextEditorController,
+    type TextEditorControllerProps,
 } from "./text_editor_controller";
 
 type HTMLElementProps = DetailedHTMLProps<
-  InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
 >;
 
 export type TextEditorProps = Omit<HTMLElementProps, "ref"> & {
-  name?: string;
-  ref?: Ref<TextEditorController>;
+    name?: string;
+    ref?: Ref<TextEditorController>;
 } & TextEditorControllerProps;
 
 export function TextEditor({
-  ref,
-  name,
-  className,
-  autoFocus,
-  onChange,
-  mode,
-  state,
-  editor,
-  defaultValue,
-  updateDelay,
-  placeholder,
-  attachFile,
-  style,
-  ...props
+    ref,
+    name,
+    className,
+    autoFocus,
+    onChange,
+    mode,
+    state,
+    editor,
+    defaultValue,
+    updateDelay,
+    placeholder,
+    attachFile,
+    style,
+    ...props
 }: TextEditorProps = {}) {
-  const containerRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
 
-  const controller = useMemo(
-    () =>
-      new TextEditorController({
-        mode,
-        state,
-        editor,
-        autoFocus,
-        placeholder,
-        updateDelay,
-        defaultValue,
-        attachFile,
-        style,
-      }),
-    [],
-  );
+    const controller = useMemo(
+        () =>
+            new TextEditorController({
+                mode,
+                state,
+                editor,
+                autoFocus,
+                placeholder,
+                updateDelay,
+                defaultValue,
+                attachFile,
+                style,
+            }),
+        [],
+    );
 
-  useImperativeHandle(ref, () => controller, [controller]);
+    useImperativeHandle(ref, () => controller, [controller]);
 
-  useEffect(() => {
-    const container = containerRef.current;
+    useLayoutEffect(() => {
+        const container = containerRef.current;
 
-    if (!container) {
-      return;
-    }
+        if (!container) {
+            return;
+        }
 
-    controller.bind(container);
+        controller.bind(container);
 
-    return () => {
-      controller.dispose();
-    };
-  }, [controller]);
+        return () => {
+            controller.dispose();
+        };
+    }, [controller]);
 
-  return (
-    <>
-      <div {...props} ref={containerRef} className={className} />
-      <TextEditorInput
-        name={name}
-        controller={controller}
-        onChange={onChange}
-      />
-    </>
-  );
+    return (
+        <>
+            <div {...props} ref={containerRef} className={className} />
+            <TextEditorInput
+                name={name}
+                controller={controller}
+                onChange={onChange}
+            />
+        </>
+    );
 }
