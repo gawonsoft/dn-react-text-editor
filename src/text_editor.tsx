9e65fee679f12a800d1b3f2938e881ec4bfa222f
oh-jinsu <ohjinsu98@icloud.com>
2026-03-05 15:25:06 +0900

    Pass the schema

--- src/text_editor.tsx
+++ src/text_editor.tsx
@@ -1,87 +1,89 @@
 import {
-    useImperativeHandle,
-    useLayoutEffect,
-    useMemo,
-    type DetailedHTMLProps,
-    type InputHTMLAttributes,
-    type Ref,
+  useImperativeHandle,
+  useLayoutEffect,
+  useMemo,
+  type DetailedHTMLProps,
+  type InputHTMLAttributes,
+  type Ref,
 } from "react";
 import { useRef } from "react";
 import { TextEditorInput } from "./input";
 import {
-    TextEditorController,
-    type TextEditorControllerProps,
+  TextEditorController,
+  type TextEditorControllerProps,
 } from "./text_editor_controller";
 
 type HTMLElementProps = DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
 >;
 
 export type TextEditorProps = Omit<HTMLElementProps, "ref" | "onChange"> & {
-    name?: string;
-    ref?: Ref<TextEditorController>;
-    onChange?: Parameters<typeof TextEditorInput>[0]["onChange"];
+  name?: string;
+  ref?: Ref<TextEditorController>;
+  onChange?: Parameters<typeof TextEditorInput>[0]["onChange"];
 } & TextEditorControllerProps;
 
 export function TextEditor({
-    ref,
-    name,
-    className,
-    autoFocus,
-    onChange,
-    mode,
-    state,
-    editor,
-    defaultValue,
-    updateDelay,
-    placeholder,
-    attachFile,
-    style,
-    ...props
+  ref,
+  name,
+  className,
+  autoFocus,
+  onChange,
+  schema,
+  mode,
+  state,
+  editor,
+  defaultValue,
+  updateDelay,
+  placeholder,
+  attachFile,
+  style,
+  ...props
 }: TextEditorProps = {}) {
-    const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    const controller = useMemo(
-        () =>
-            new TextEditorController({
-                mode,
-                state,
-                editor,
-                autoFocus,
-                placeholder,
-                updateDelay,
-                defaultValue,
-                attachFile,
-                style,
-            }),
-        [],
-    );
+  const controller = useMemo(
+    () =>
+      new TextEditorController({
+        schema,
+        mode,
+        state,
+        editor,
+        autoFocus,
+        placeholder,
+        updateDelay,
+        defaultValue,
+        attachFile,
+        style,
+      }),
+    [],
+  );
 
-    useImperativeHandle(ref, () => controller, [controller]);
+  useImperativeHandle(ref, () => controller, [controller]);
 
-    useLayoutEffect(() => {
-        const container = containerRef.current;
+  useLayoutEffect(() => {
+    const container = containerRef.current;
 
-        if (!container) {
-            return;
-        }
+    if (!container) {
+      return;
+    }
 
-        controller.bind(container);
+    controller.bind(container);
 
-        return () => {
-            controller.dispose();
-        };
-    }, [controller]);
+    return () => {
+      controller.dispose();
+    };
+  }, [controller]);
 
-    return (
-        <>
-            <div {...props} ref={containerRef} className={className} />
-            <TextEditorInput
-                name={name}
-                controller={controller}
-                onChange={onChange}
-            />
-        </>
-    );
+  return (
+    <>
+      <div {...props} ref={containerRef} className={className} />
+      <TextEditorInput
+        name={name}
+        controller={controller}
+        onChange={onChange}
+      />
+    </>
+  );
 }
